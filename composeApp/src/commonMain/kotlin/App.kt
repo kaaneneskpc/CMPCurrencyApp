9dfdaa8fb9d77e1bc7fc4fa04bf9ab2cbddf5686
kkapici <kkapici@innova.com.tr>
2024-04-27 13:51:59 +0300

    - Fixed Desktop and Android App initializeKoin() state

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,26 +1,17 @@
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import cafe.adriel.voyager.navigator.Navigator
-import com.russhwolf.settings.Settings
-import data.local.MongoImpl
-import data.local.PreferencesImpl
-import data.remote.api.CurrencyApiServiceImpl
-import di.appModule
 import di.initializeKoin
-import domain.CurrencyApiService
-import domain.MongoRepository
-import domain.PreferencesRepository
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.core.context.startKoin
-import org.koin.dsl.module
 import presentation.screen.HomeScreen
-import presentation.screen.HomeViewModel
-import java.awt.Desktop
 
 
 @Composable
 @Preview
 fun App() {
+    if(getPlatform().name == "Android") {
+        initializeKoin()
+    }
     MaterialTheme {
         Navigator(HomeScreen())
     }
